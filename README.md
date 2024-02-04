405cf51206377661797f14c17ccd91605f4f6afb
IA1phaI <Pavel.2014.play@gmail.com>
2024-02-04 14:24:18 +0300

    back

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:dea9e91214d91cc924358363bc909cc55c3530177680d309f09e6a2a7abbd267
-size 43
+# calculator3000
+My Calculator on JavaFX
--- src/main/java/ru/a1pha/Start.java
+++ src/main/java/ru/a1pha/Start.java
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:45aa400890fdeba61c2268fd62ea8b6f7edae1731597b9d544264f65d820eca5
-size 124
+package ru.a1pha;
+
+public class Start {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}

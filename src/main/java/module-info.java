6e657d026174b5697a68665c45257c0e94ebf498
IA1phaI <Pavel.2014.play@gmail.com>
2024-02-10 15:18:30 +0300

    update module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module ru.a1pha {
     requires javafx.controls;
+    requires javafx.base;
     requires javafx.fxml;
-
+    requires javafx.graphics;
+    
     opens ru.a1pha to javafx.fxml;
     exports ru.a1pha;
 }
